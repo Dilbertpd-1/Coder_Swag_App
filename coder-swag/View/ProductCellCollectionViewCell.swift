680d9bcea9aa217672b419c67d7b46501e0d19d2
Aadil Bachh <aadilbachh@icloud.com>
2020-05-12 17:42:33 +0100

    lesson 3 complete

---
+++ coder-swag/View/ProductCellCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCellCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by Aadil Bachh on 12/05/2020.
+//  Copyright © 2020 Aadil Bachh. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
